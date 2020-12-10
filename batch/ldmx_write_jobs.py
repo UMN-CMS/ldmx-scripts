3154fac7193514edb7571ca7b115e862fd5bc0eb
tomeichlersmith <eichl008@umn.edu>
2020-12-10 09:29:07 -0600

    initial commit of important scripts

---
+++ batch/ldmx_write_jobs.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python
+
+import os,sys
+import argparse
+import subprocess
+
+parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+# required arg
+parser.add_argument("job_list",type=str,help="Name of job list file that we should write to.")
+parser.add_argument("-c","--config",required=True,type=str,help="Config script to run.")
+parser.add_argument("-o","--out_dir",required=True,type=str,help="Directory to copy output to.")
+environment = parser.add_mutually_exclusive_group(required=True)
+environment.add_argument("--env_script",type=str,default=None,help="Environment script to run before running fire.")
+environment.add_argument("--ldmx_version",type=str,default=None,help="LDMX Version to pick a pre-made environment script.")
+
+# optional arg
+parser.add_argument("--input_dir",default=None,type=str,help="Directory containing input files to run over.")
+parser.add_argument("--num_jobs",type=int,default=None,help="Number of jobs to run (if not input directory given).")
+parser.add_argument("--config_args",type=str,default='',help="Extra arguments to be passed to the configuration script.")
+parser.add_argument("--start_job",type=int,default=0,help="Starting number to use when counting jobs (and run numbers)")
+
+# rarely-used optional args
+parser.add_argument("-t","--test",action='store_true',dest='test',help="Don't submit the job to the batch.")
+parser.add_argument("--nonice",action='store_true',dest="nonice",help="Do not run this at nice priority.")
+parser.add_argument("--run_script",type=str,help="Script to run jobs on worker nodes with.",default='%s/run_fire.sh'%os.path.dirname(os.path.realpath(__file__)))
+
+arg = parser.parse_args()
+
+jobs = 0
+if arg.input_dir is not None :
+    full_input_dir = os.path.realpath(arg.input_dir)
+    inputFileList = os.listdir(arg.input_dir)
+    if arg.num_jobs is not None :
+        jobs = min(arg.num_jobs,len(inputFileList))
+    else :
+        jobs = len(inputFileList)
+elif arg.num_jobs is not None :
+    jobs = arg.num_jobs
+else :
+    parser.error("Either an input directory of files or a number of jobs needs to be given.")
+
+# make the output directory
+full_out_dir_path = os.path.realpath(arg.out_dir)
+if not os.path.exists(full_out_dir_path):
+    os.makedirs(full_out_dir_path)
+
+if not os.path.exists(full_out_dir_path):
+    raise Exception('Unable to create output directory "%s"'%full_out_dir_path)
+
+full_config_path = os.path.realpath(arg.config)
+if not os.path.exists(full_config_path) :
+    raise Exception('Config script "%s" does not exist.'%full_config_path)
+
+if arg.env_script is not None :
+    env_script = os.path.realpath(arg.env_script)
+elif arg.ldmx_version is not None :
+    env_script = "/local/cms/user/eichl008/ldmx/stable-installs/%s/setup.sh"%arg.ldmx_version
+else :
+    parser.error('Either a full env script \'--env_script\' or a ldmx_version \'--ldmx_version\' must be specified.')
+
+if not os.path.exists(env_script) :
+    raise Exception('Environment script "%s" does not exist.'%env_script)
+
+# This needs to match the correct order of the arguments in the run_fire.sh script
+#   The input file and any extra config arguments are optional and come after the
+#   three required arguments
+arguments_template = '{env_script} {config_script} {out_dir}'
+
+# Write Condor submit file 
+with open(arg.job_list,'w') as job_sub_file :
+    job_sub_file.write("Executable          =  %s\n"%os.path.realpath(arg.run_script))
+    job_sub_file.write("Universe            =  vanilla\n")
+    job_sub_file.write("Requirements        =  Arch==\"X86_64\"  &&  (Machine  !=  \"zebra02.spa.umn.edu\")  &&  (Machine  !=  \"zebra03.spa.umn.edu\")  &&  (Machine  !=  \"zebra04.spa.umn.edu\")\n")
+    job_sub_file.write("+CondorGroup        =  \"cmsfarm\"\n")
+    if not arg.nonice:
+        job_sub_file.write("nice_user = True\n")
+    job_sub_file.write("Request_Memory      =  1 Gb\n")
+    
+    for job in range(arg.start_job,arg.start_job+arg.num_jobs) :
+        arguments = arguments_template.format(
+                env_script = env_script,
+                config_script = full_config_path,
+                out_dir = full_out_dir_path
+                )
+
+        if arg.input_dir is not None :
+            arguments += ' %s'%input_file_list[job-arg.start_job]
+
+        arguments += ' --run_number %d %s'%(job,arg.config_args)
+
+        if arg.test :
+            job_sub_file.write('output = %s/%d.out\n'%(full_out_dir_path,job))
+            job_sub_file.write('error = %s/%d.err\n'%(full_out_dir_path,job))
+
+        job_sub_file.write('arguments = %s\n'%arguments)
+        job_sub_file.write('queue\n')
+    #end loop over jobs
+#submit job list is open
---
+++ ldmx-env.sh
@@ -0,0 +1,98 @@
+#!/bin/bash
+
+# SUGGESTION:
+#   Put the following command in your .bashrc file to make setting up
+#   the ldmx environment easier
+#   alias ldmxenv='source <path-to-this-file>/ldmx-env-umn.sh'
+
+# This is the full path to the directory containing ldmx-sw
+export LDMXBASE="/home/eichl008/local-home/ldmx"
+
+# installation prefix for ldmx-sw
+export LDMX_INSTALL_PREFIX="$LDMXBASE/ldmx-sw/install"
+
+### Helpful Aliases and Bash Functions
+# cmake script required to be done before make to build ldmx-sw
+# WARNING: must be in $LDMXBASE/ldmx-sw/build directory when you run cmake
+#   if you run it outside of build directory and it completes, 
+#   you will need to git reset and git clean to remove
+#   the build files that are mixed with the source files
+function ldmxcmake {
+  (set -x; cmake -DCMAKE_INSTALL_PREFIX=$LDMX_INSTALL_PREFIX -DBOOST_ROOT=$BOOSTDIR "$@" ../;)
+}
+
+function ldmxana-cmake {
+  (set -x; cmake -DBOOST_ROOT=$BOOSTDIR -DLDMXSW_INSTALL_PREFIX=$LDMX_INSTALL_PREFIX "$@" ../;)
+}
+
+# prepends ldmx-app with valgrind to check for memory leaks
+alias ldmx-val='valgrind --tool=memcheck --leak-check=yes --suppressions=$ROOTSYS/etc/root/valgrind-root.supp --log-file=memcheck.log ldmx-app'
+
+# skips directories that aren't modules
+#   now run 'grepmodules <pattern>' in ldmx-sw/ to search for a <pattern> in the module source files
+alias grepmodules='grep --exclude-dir=build --exclude-dir=docs --exclude-dir=install --exclude-dir=.git -rHn'
+
+# total remake command
+# nuclear option
+#   deletes ldmx install
+#   goes to build directory, completely deletes old build, 
+#   re-executes cmake and make, returns to prior directory
+ldmxremake() {
+  rm -rf $LDMX_INSTALL_PREFIX &&
+  cd $LDMXBASE/ldmx-sw/build &&
+  rm -r * &&
+  ldmxcmake &&
+  make install -j8 &&
+  cd -
+}
+
+### The rest is believed to be the same for all umn users
+# It is a hassle to change the gcc version because all of the other
+# libraries could change versions. This means when we want to change
+# the gcc version, you must go through the cvmfs directory tree and
+# input the versions of these other libraries that we need. There is
+# almost certainly an intelligent bash script that could do this for
+# us, but I am not writing one write now.
+# You also need to touch these library directories so that they
+# appear when cmake looks for them.
+
+
+## bash variables needed by cmake
+# /local/cms installations of geant4 and root
+TMP_ROOT="/local/cms/user/eichl008/install-test"
+G4_VERSION_10_2_gcc821_cxx17="geant4.10.02.p03_v0.3-gcc821-cxx17"
+G4_VERSION_10_2_gcc821="geant4.10.02.p03_v0.3-gcc821"
+G4_VERSION_10_5_gcc821="geant4.10.05.p01-gcc821" 
+export G4DIR="$TMP_ROOT/local/cms/other/geant4/$G4_VERSION_10_2_gcc821"
+ROOT_VERSION_6_16_gcc821_cxx17="6.16.00-gcc821-cxx17"
+ROOT_VERSION_6_16_gcc821="6.16.00-gcc821"
+ROOT_VERSION_6_20_gcc821_cxx17="6.20.00-gcc821-cxx17"
+ROOTDIR="$TMP_ROOT/local/cms/other/root/$ROOT_VERSION_6_20_gcc821_cxx17"
+export BOOSTDIR="/local/cms/user/eichl008/boost/install"
+export PYTHONHOME="/local/cms/user/eichl008/python/install"
+
+# location of cms shared libraries
+# use this to specifiy which gcc should be used in compilation
+CVMFSDIR="/cvmfs/cms.cern.ch/slc7_amd64_gcc820"
+export XERCESDIR="$CVMFSDIR/external/xerces-c/3.1.3"
+export GCCDIR="$CVMFSDIR/external/gcc/8.2.0"
+
+## Initialize libraries/programs from cvmfs and /local/cms
+# all of these init scripts add their library paths to LD_LIBRARY_PATH
+source $XERCESDIR/etc/profile.d/init.sh                         #xerces-c
+source $CVMFSDIR/external/cmake/3.17.2/etc/profile.d/init.sh    #cmake
+source $CVMFSDIR/external/bz2lib/1.0.6/etc/profile.d/init.sh    #bz2lib
+source $CVMFSDIR/external/zlib/1.0/etc/profile.d/init.sh        #zlib
+source $GCCDIR/etc/profile.d/init.sh                            #gcc
+source $ROOTDIR/bin/thisroot.sh                                 #root 
+source $G4DIR/bin/geant4.sh                                     #geant4
+
+# add libraries to cmake/make search path for linking
+export LD_LIBRARY_PATH=$LDMX_INSTALL_PREFIX/lib:$LDMX_INSTALL_PREFIX/external/onnxruntime-linux-x64-1.2.0/lib:$BOOSTDIR/lib:$PYTHONHOME/lib:$LD_LIBRARY_PATH
+
+# add ldmx python scripts to python search path
+export PYTHONPATH=$PYTHONHOME/lib/python3:$LDMX_INSTALL_PREFIX/python:$LDMXBASE/eat/install/lib/python:$PYTHONPATH
+
+# add ldmx executables to system search path
+export PATH=$PYTHONHOME/bin:$LDMX_INSTALL_PREFIX/bin:$PATH
+

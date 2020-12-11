07a10a62ad2d3fd64c02d22060c33038d1dae67e
tomeichlersmith <eichl008@umn.edu>
2020-12-11 10:08:49 -0600

    cleanup, move class def to top side

--- batch/ldmx_write_jobs.py
+++ batch/ldmx_write_jobs.py
@@ -1,68 +1,4 @@
 
-import os,sys
-import argparse
-import subprocess
-
-parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-
-# required arg
-parser.add_argument("job_list",type=str,help="Name of job list file that we should write to.")
-parser.add_argument("-c","--config",required=True,type=str,help="Config script to run.")
-parser.add_argument("-o","--out_dir",required=True,type=str,help="Directory to copy output to.")
-environment = parser.add_mutually_exclusive_group(required=True)
-environment.add_argument("--env_script",type=str,default=None,help="Environment script to run before running fire.")
-environment.add_argument("--ldmx_version",type=str,default=None,help="LDMX Version to pick a pre-made environment script.")
-
-# optional arg
-parser.add_argument("--input_dir",default=None,type=str,help="Directory containing input files to run over.")
-parser.add_argument("--num_jobs",type=int,default=None,help="Number of jobs to run (if not input directory given).")
-parser.add_argument("--config_args",type=str,default='',help="Extra arguments to be passed to the configuration script.")
-parser.add_argument("--start_job",type=int,default=0,help="Starting number to use when counting jobs (and run numbers)")
-
-# rarely-used optional args
-parser.add_argument("-t","--test",action='store_true',dest='test',help="Don't submit the job to the batch.")
-parser.add_argument("--nonice",action='store_true',dest="nonice",help="Do not run this at nice priority.")
-parser.add_argument("--run_script",type=str,help="Script to run jobs on worker nodes with.",default='%s/run_fire.sh'%os.path.dirname(os.path.realpath(__file__)))
-parser.add_argument("--tmp_root",type=str,help="Directory to create any working directories inside of.",default='/export/scratch/user/%s/'%os.environ['USER'])
-
-arg = parser.parse_args()
-
-jobs = 0
-if arg.input_dir is not None :
-    full_input_dir = os.path.realpath(arg.input_dir)
-    input_file_list = os.listdir(arg.input_dir)
-    if arg.num_jobs is not None :
-        jobs = min(arg.num_jobs,len(input_file_list))
-    else :
-        jobs = len(input_file_list)
-elif arg.num_jobs is not None :
-    jobs = arg.num_jobs
-else :
-    parser.error("Either an input directory of files or a number of jobs needs to be given.")
-
-# make the output directory
-full_out_dir_path = os.path.realpath(arg.out_dir)
-if not os.path.exists(full_out_dir_path):
-    os.makedirs(full_out_dir_path)
-
-if not os.path.exists(full_out_dir_path):
-    raise Exception('Unable to create output directory "%s"'%full_out_dir_path)
-
-full_config_path = os.path.realpath(arg.config)
-if not os.path.exists(full_config_path) :
-    raise Exception('Config script "%s" does not exist.'%full_config_path)
-
-if arg.env_script is not None :
-    env_script = os.path.realpath(arg.env_script)
-elif arg.ldmx_version is not None :
-    env_script = "/local/cms/user/%s/ldmx/stable-installs/%s/setup.sh"%(os.environ['USER'],arg.ldmx_version)
-else :
-    parser.error('Either a full env script \'--env_script\' or a ldmx_version \'--ldmx_version\' must be specified.')
-
-if not os.path.exists(env_script) :
-    raise Exception('Environment script "%s" does not exist.'%env_script)
-
-
 class condor_submit_file() :
     """A wrapper for writing a condor submit file.
   
@@ -125,6 +61,69 @@ on_exit_hold        = (ExitCode != 0)
         self._file.write('next_job_start_delay = %d\n'%pause)
         self._file.write('queue\n')
 
+import os,sys
+import argparse
+
+parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+# required arg
+parser.add_argument("job_list",type=str,help="Name of job list file that we should write to.")
+parser.add_argument("-c","--config",required=True,type=str,help="Config script to run.")
+parser.add_argument("-o","--out_dir",required=True,type=str,help="Directory to copy output to.")
+environment = parser.add_mutually_exclusive_group(required=True)
+environment.add_argument("--env_script",type=str,default=None,help="Environment script to run before running fire.")
+environment.add_argument("--ldmx_version",type=str,default=None,help="LDMX Version to pick a pre-made environment script.")
+
+# optional arg
+parser.add_argument("--input_dir",default=None,type=str,help="Directory containing input files to run over.")
+parser.add_argument("--num_jobs",type=int,default=None,help="Number of jobs to run (if not input directory given).")
+parser.add_argument("--config_args",type=str,default='',help="Extra arguments to be passed to the configuration script.")
+parser.add_argument("--start_job",type=int,default=0,help="Starting number to use when counting jobs (and run numbers)")
+
+# rarely-used optional args
+parser.add_argument("-t","--test",action='store_true',dest='test',help="Don't submit the job to the batch.")
+parser.add_argument("--nonice",action='store_true',dest="nonice",help="Do not run this at nice priority.")
+parser.add_argument("--run_script",type=str,help="Script to run jobs on worker nodes with.",default='%s/run_fire.sh'%os.path.dirname(os.path.realpath(__file__)))
+parser.add_argument("--tmp_root",type=str,help="Directory to create any working directories inside of.",default='/export/scratch/user/%s/'%os.environ['USER'])
+parser.add_argument("--sleep",type=int,help="Time in seconds to sleep before starting the next job.",default=60)
+
+arg = parser.parse_args()
+
+jobs = 0
+if arg.input_dir is not None :
+    full_input_dir = os.path.realpath(arg.input_dir)
+    input_file_list = os.listdir(arg.input_dir)
+    if arg.num_jobs is not None :
+        jobs = min(arg.num_jobs,len(input_file_list))
+    else :
+        jobs = len(input_file_list)
+elif arg.num_jobs is not None :
+    jobs = arg.num_jobs
+else :
+    parser.error("Either an input directory of files or a number of jobs needs to be given.")
+
+# make the output directory
+full_out_dir_path = os.path.realpath(arg.out_dir)
+if not os.path.exists(full_out_dir_path):
+    os.makedirs(full_out_dir_path)
+
+if not os.path.exists(full_out_dir_path):
+    raise Exception('Unable to create output directory "%s"'%full_out_dir_path)
+
+full_config_path = os.path.realpath(arg.config)
+if not os.path.exists(full_config_path) :
+    raise Exception('Config script "%s" does not exist.'%full_config_path)
+
+if arg.env_script is not None :
+    env_script = os.path.realpath(arg.env_script)
+elif arg.ldmx_version is not None :
+    env_script = "/local/cms/user/%s/ldmx/stable-installs/%s/setup.sh"%(os.environ['USER'],arg.ldmx_version)
+else :
+    parser.error('Either a full env script \'--env_script\' or a ldmx_version \'--ldmx_version\' must be specified.')
+
+if not os.path.exists(env_script) :
+    raise Exception('Environment script "%s" does not exist.'%env_script)
+
 job_sub_file = condor_submit_file(arg.job_list,arg.run_script,not arg.nonice)
     
 # This needs to match the correct order of the arguments in the run_fire.sh script
@@ -144,5 +143,5 @@ for job in range(arg.start_job,arg.start_job+jobs) :
 
     arguments += ' --run_number %d %s'%(job,arg.config_args)
 
-    job_sub_file.add(arguments,60,arg.test)
+    job_sub_file.add(arguments,arg.sleep,arg.test)
 #end loop over jobs

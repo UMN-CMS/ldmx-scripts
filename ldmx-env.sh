09edb25b9af00e64e476871bedf3b44804e93591
tomeichlersmith <eichl008@umn.edu>
2020-12-29 10:25:16 -0600

    fix up environment script to check if cvmfs is connected

--- ldmx-env.sh
+++ ldmx-env.sh
@@ -3,7 +3,7 @@
 # SUGGESTION:
 #   Put the following command in your .bashrc file to make setting up
 #   the ldmx environment easier
-#   alias ldmxenv='source <path-to-this-file>/ldmx-env.sh'
+#   alias ldmx-env='source <path-to-this-file>/ldmx-env.sh'
 
 # get the directory of this script
 export LDMX_ENV_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &>/dev/null && pwd )"
@@ -72,8 +72,13 @@ export G4DIR="$TMP_ROOT/local/cms/other/geant4/$G4_VERSION_10_2_gcc821"
 ROOT_VERSION_6_16_gcc821_cxx17="6.16.00-gcc821-cxx17"
 ROOT_VERSION_6_16_gcc821="6.16.00-gcc821"
 ROOT_VERSION_6_20_gcc821_cxx17="6.20.00-gcc821-cxx17"
+ROOT_VERSION_6_22_gcc821_cxx17="6.22.00-gcc821-cxx17"
 ROOTDIR="$TMP_ROOT/local/cms/other/root/$ROOT_VERSION_6_20_gcc821_cxx17"
 export BOOSTDIR="/local/cms/user/eichl008/boost/install"
+
+# Using the bash variable 'PYTHONHOME' gives a hint to look
+#   here for Python to cmake, so this helps simplify our
+#   cmake commands.
 export PYTHONHOME="/local/cms/user/eichl008/python/install"
 
 # location of cms shared libraries
@@ -82,22 +87,67 @@ CVMFSDIR="/cvmfs/cms.cern.ch/slc7_amd64_gcc820"
 export XERCESDIR="$CVMFSDIR/external/xerces-c/3.1.3"
 export GCCDIR="$CVMFSDIR/external/gcc/8.2.0"
 
+# sometimes our computers are disconnected from cvmfs
+#   so we need to check if we found the necessary source files
+_we_good="YES"
+
+# Setup the input package
+#   if the path contains 'cvmfs', then we assume we are given
+#     a path to cvmfs package and source the corresponding init.sh
+#   otherwise, source the input
+ldmx-env-source() {
+  _file_to_source="$1"
+  if [[ "$1" == *"cvmfs"* ]]
+  then
+    _file_to_source=$1/etc/profile.d/init.sh
+  fi
+
+  if ! source $_file_to_source
+  then
+    _we_good="$_file_to_source"
+  fi
+}
+
 ## Initialize libraries/programs from cvmfs and /local/cms
 # all of these init scripts add their library paths to LD_LIBRARY_PATH
-source $XERCESDIR/etc/profile.d/init.sh                         #xerces-c
-source $CVMFSDIR/external/cmake/3.17.2/etc/profile.d/init.sh    #cmake
-source $CVMFSDIR/external/bz2lib/1.0.6/etc/profile.d/init.sh    #bz2lib
-source $CVMFSDIR/external/zlib/1.0/etc/profile.d/init.sh        #zlib
-source $GCCDIR/etc/profile.d/init.sh                            #gcc
-source $ROOTDIR/bin/thisroot.sh                                 #root 
-source $G4DIR/bin/geant4.sh                                     #geant4
+ldmx-env-source $XERCESDIR                      #xerces-c
+ldmx-env-source $CVMFSDIR/external/cmake/3.17.2 #cmake
+ldmx-env-source $CVMFSDIR/external/bz2lib/1.0.6 #bz2lib
+ldmx-env-source $CVMFSDIR/external/zlib/1.0     #zlib
+ldmx-env-source $GCCDIR                         #gcc
+ldmx-env-source $ROOTDIR/bin/thisroot.sh        #root 
+ldmx-env-source $G4DIR/bin/geant4.sh            #geant4
+
+if [ $_we_good != "YES" ]
+then
+  echo "Could not source '$_we_good'! (Most likely cvmfs has been disconnected.)"
+  return 1
+fi
+
+# Make the directory passed to this funtion
+#   another directory that could be loaded by a program at runtime
+#
+# Adds <input-dir>/lib to LD_LIBRARY_PATH and <input-dir>/bin to PATH
+ldmx-env-load-lib() {
+  export LD_LIBRARY_PATH="$1"/lib:$LD_LIBRARY_PATH
+  export PATH="$1"/bin:$PATH
+}
 
 # add libraries to cmake/make search path for linking
-export LD_LIBRARY_PATH=$LDMX_INSTALL_PREFIX/lib:$LDMX_INSTALL_PREFIX/external/onnxruntime-linux-x64-1.2.0/lib:$BOOSTDIR/lib:$PYTHONHOME/lib:$LD_LIBRARY_PATH
+ldmx-env-load-lib $PYTHONHOME
+ldmx-env-load-lib $BOOSTDIR
+ldmx-env-load-lib $LDMX_INSTALL_PREFIX
+ldmx-env-load-lib $LDMX_INSTALL_PREFIX/external/onnxruntime-linux-x64-1.2.0
+
+# Load the python module passed to this function
+#   when running a python program
+#
+# Adds <input-dir> to PYTHONPATH
+ldmx-env-load-module() {
+  export PYTHONPATH="$1":$PYTHONPATH
+}
 
 # add ldmx python scripts to python search path
-export PYTHONPATH=$PYTHONHOME/lib/python3:$LDMX_INSTALL_PREFIX/python:$PYTHONPATH
-
-# add ldmx executables to system search path
-export PATH=$PYTHONHOME/bin:$LDMX_INSTALL_PREFIX/bin:$PATH
+ldmx-env-load-module $PYTHONHOME/lib/python3
+ldmx-env-load-module $LDMX_INSTALL_PREFIX/python
 

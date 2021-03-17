414aff8f8c57d99e90368bb4f5e026a6a4048c59
tomeichlersmith <eichl008@umn.edu>
2021-03-17 12:34:07 -0500

    transition environment script to container-based

--- ldmx-env.sh
+++ ldmx-env.sh
@@ -3,37 +3,19 @@
 # SUGGESTION:
 #   Put the following command in your .bashrc file to make setting up
 #   the ldmx environment easier
-#   alias ldmx-env='source <path-to-this-file>/ldmx-env.sh'
+#   alias ldmx-env='source <path-to-this-file>/ldmx-env.sh; unalias ldmx-env'
 
 # get the directory of this script
 export LDMX_ENV_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &>/dev/null && pwd )"
 
 # This is the full path to the directory containing ldmx-sw
-export LDMX_BASE="$( cd "${LDMX_ENV_DIR}/../" &>/dev/null && pwd)"
+_base="$( cd "${LDMX_ENV_DIR}/../" &>/dev/null && pwd)"
 
-# installation prefix for ldmx-sw
-export LDMX_INSTALL_PREFIX="$LDMX_BASE/ldmx-sw/install"
-
-## Setup LDMX dependencies
-if ! source ${LDMX_ENV_DIR}/ldmx-setup-deps.sh
-then
-  echo "Error setting up dependencies."
+# Setup container environment
+if ! source $_base/ldmx-sw/scripts/ldmx-env.sh -b $_base $@; then
+  echo "ERROR from container setup script."
   return 1
 fi
-
-### Helpful Aliases and Bash Functions
-# cmake command required to be done before make to build ldmx-sw
-# WARNING: must be in $LDMX_BASE/ldmx-sw/build directory when you run cmake
-#   if you run it outside of build directory and it completes, 
-#   you will need to git reset and git clean to remove
-#   the build files that are mixed with the source files
-function ldmx-cmake {
-  (set -x; cmake -DCMAKE_INSTALL_PREFIX=$LDMX_INSTALL_PREFIX "$@" ../;)
-}
-
-function ldmx-ana-cmake {
-  (set -x; cmake -DLDMXSW_INSTALL_PREFIX=$LDMX_INSTALL_PREFIX "$@" ../;)
-}
 
 # prepends fire with valgrind to check for memory leaks
 alias ldmx-val='valgrind --tool=memcheck --leak-check=yes --suppressions=$ROOTSYS/etc/root/valgrind-root.supp --log-file=memcheck.log fire'
@@ -43,7 +25,42 @@ alias ldmx-val='valgrind --tool=memcheck --leak-check=yes --suppressions=$ROOTSY
 alias grepmodules='grep --exclude-dir=build --exclude-dir=docs --exclude-dir=install --exclude-dir=.git -rHn'
 
 # setup the condor batch environemtn
-alias ldmx-condor-env='source $LDMX_ENV_DIR/batch/condor_env.sh'
+alias ldmx-condor-env='source $LDMX_ENV_DIR/batch/condor_env.sh; unalias ldmx-condor-env'
 
 # remove all the files listed in install_manifest.txt
 alias ldmx-clean-install='xargs rm < install_manifest.txt'
+
+# A safe copy where we make sure that the copy succeeded
+safe-cp() {
+  local _file="$1"
+  local _dest_dir="$2"
+  echo -n "$_file..."
+  if cp -t $_dest_dir $_file; then
+    sync
+    if cmp -s $_dest_dir/$_file $_file; then
+      echo "success!"
+      return 0
+    else
+      echo "failed on cmp"
+      #remove non-matching destination file
+      rm $_dest_dir/$_file
+    fi
+  else
+    echo "failed on cp"
+    #remove failed copy (if it exists)
+    # sometimes cp leaves a partially copied file behind
+    if [[ -f $_dest_dir/$_file ]]; then
+      rm $_dest_dir/$_file;
+    fi
+  fi
+  return 1
+}
+
+# Safely move a file, making sure the copy succeeded
+safe-mv() {
+  local _file="$1"
+  local _dest_dir="$2"
+  if safe-cp $_file $_dest_dir; then
+    rm $_file
+  fi
+}
